a90a99c333d7c8b4531fc7410a3134e542313183
GoldBeardSea <timbusch1@gmail.com>
2018-07-26 12:26:03 -0700

    had to import another application to fix server crashing issues

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -2,17 +2,16 @@ package server;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import server.storage.FileSystemStorageService;
 
-import javax.swing.*;
-
-@SpringBootConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
         System.out.println("http://localhost:8080");
     }
 
