be506d1ccd08addb7f938bf5e91675ce18551768
Panos <panosrockbear@gmail.com>
2018-09-13 22:21:11 -0700

    working, no tests

---
+++ lab-panos/readme.md
@@ -0,0 +1,4 @@
+This app takes in a .txt file and returns a count of how many words are within as well as an estimate of how difficult the text is to read.
+
+examples:
+
---
+++ lab-panos/src/main/java/com/codefellows/labpanos/LabPanosApplication.java
@@ -0,0 +1,12 @@
+package com.codefellows.labpanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPanosApplication.class, args);
+	}
+}
